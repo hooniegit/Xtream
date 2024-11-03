c0b94e7ffd5ff99957b1735544d4779515b1fafa
hooniegit <neivekim76@gmail.com>
2024-11-03 15:25:59 +0900

    241103 feat: Basement

---
+++ Xtream/src/main/java/com/hooniegit/Xtream/XtreamApplication.java
@@ -0,0 +1,13 @@
+package com.hooniegit.Xtream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XtreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XtreamApplication.class, args);
+	}
+
+}
